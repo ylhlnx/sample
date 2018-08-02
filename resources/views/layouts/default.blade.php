cdb1069fdd7d42bb2676f74260140f43041c898f
ylh <ylhlnx@163.com>
2018-08-02 12:45:08 +0000

    Finish static pages

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>@yield('title','sample')</title>
+  </head>
+  <style type="text/css">
+      h1{
+        text-align: center;
+      }
+  </style>
+  <body>
+    @yield('content')
+  </body>
+</html>
